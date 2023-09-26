07051f184afd956124c60462fcf314c312fdca68
AlejandroEN <alejandroescobarnunez@gmail.com>
2023-09-25 21:18:52 -0500

    comments added

--- main.cpp
+++ main.cpp
@@ -4,24 +4,28 @@
  * - Ulrich Nieto Rosales (teoría 2)
  * - Ariana Mercado Barbieri (teoría 1)
  */
-
 #include <iostream>
 #include "functions.hpp"
 
 int main() {
     // tanque = {cantidad actual de agua, cantidad de bombeos, cantidad de reparticiones}
-    auto *tanqueA = new unsigned int[3] {10, 5, 0};
-    auto *tanqueB = new unsigned int[3] {10, 0, 0};
+    unsigned int tanqueA[3] = {10, 0, 0};
+    unsigned int tanqueB[3] = {10, 0, 0};
 
+    // Simular el proceso de repartición y bombeo para tanque A
     while (repartir(tanqueA))
         bombear(tanqueA);
 
+    // Simular el proceso de repartición y bombeo para tanque B
     while (repartir(tanqueB))
         bombear(tanqueB);
 
-    unsigned int *tanqueActual = *tanqueA > *tanqueB ? tanqueA : tanqueB;
+    // Determinar cuál de los dos tanques tiene más agua
+    unsigned int *tanqueActual = (tanqueA[0] > tanqueB[0]) ? tanqueA : tanqueB;
 
-    do
+    // Seguir bombeando y repartiendo desde el tanque con más agua hasta que esté vacío
+    while (repartir(tanqueActual))
         bombear(tanqueActual);
-    while (repartir(tanqueActual));
+
+    return 0;
 }
